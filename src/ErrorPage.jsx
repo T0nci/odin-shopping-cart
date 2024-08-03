f27f8ca2855e30f7aac0baa7b16f003c9ab92fe5
T0nci <138928016+T0nci@users.noreply.github.com>
2024-08-03 23:22:05 +0200

    feat: Add ErrorPage component as an error catcher

--- src/ErrorPage.jsx
+++ src/ErrorPage.jsx
@@ -1,5 +1,17 @@
+import { useRouteError } from "react-router-dom";
+
 const ErrorPage = () => {
-  return <>Whoopsies!</>;
+  const error = useRouteError();
+
+  let errorText = "";
+  if (error.status && error.statusText) {
+    errorText = `${error.status}: ${error.statusText}`;
+  } else {
+    errorText = error;
+  }
+
+  console.error(error);
+  return <div>{errorText}</div>;
 };
 
 export default ErrorPage;
