d85af6caf663775d12796a361ffb9a9c6b2d6a40
T0nci <138928016+T0nci@users.noreply.github.com>
2024-08-18 17:41:48 +0200

    feat: Update and test App(Home) component

---
+++ src/Components/App/App.test.jsx
@@ -0,0 +1,102 @@
+import { describe, it, expect, vi } from "vitest";
+import { render, act } from "@testing-library/react";
+import App from "./App";
+
+vi.mock(import("../../helpers"), async (importOriginal) => {
+  const imports = await importOriginal();
+
+  const useCustomLocationState = () => {
+    let items = [];
+    const setItems = (newItems) => (items = newItems);
+
+    return [items, setItems];
+  };
+
+  return { ...imports, useCustomLocationState };
+});
+
+// Link needs to be mocked because it's in NavBar
+vi.mock(import("react-router-dom"), async (importOriginal) => {
+  const imports = await importOriginal();
+
+  const newLink = ({ children, to, ...props }) => {
+    return (
+      <a
+        href={to}
+        onClick={(e) => {
+          e.preventDefault();
+        }}
+        {...props}
+      >
+        {children}
+      </a>
+    );
+  };
+
+  return { ...imports, Link: newLink };
+});
+
+describe("App component", () => {
+  it("renders App", () => {
+    let container = null;
+    act(() => {
+      const { container: temp } = render(<App />);
+
+      container = temp;
+    });
+
+    expect(container).toMatchInlineSnapshot(`
+      <div>
+        <nav>
+          <h1>
+            FakeStore
+          </h1>
+          <ul>
+            <li>
+              <a
+                href="/"
+                state=""
+              >
+                Home
+              </a>
+            </li>
+            <li>
+              <a
+                href="/shop"
+                state=""
+              >
+                Shop
+              </a>
+            </li>
+          </ul>
+          <div>
+            <a
+              aria-label="Cart"
+              href="/cart"
+              state=""
+            >
+              <img
+                alt=""
+                src="/src/icons/cart.svg"
+              />
+            </a>
+            <div>
+              #
+            </div>
+          </div>
+        </nav>
+        <main>
+          <header>
+            Home
+          </header>
+          <h1>
+            Welcome to FakeStore!
+          </h1>
+          <p>
+            Discover a world of curated, high-quality products designed to make your life easier and more enjoyable. At FakeStore, we’re passionate about bringing you the latest trends and timeless essentials at unbeatable prices. Whether you’re shopping for cutting-edge gadgets, stylish fashion, or unique home decor, our diverse selection ensures that there’s something for everyone. Browse our collections, enjoy seamless shopping experiences, and let us help you find exactly what you need. Dive into FakeStore today—where exceptional finds are just a click away!
+          </p>
+        </main>
+      </div>
+    `);
+  });
+});
