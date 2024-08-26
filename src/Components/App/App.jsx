98a6cb1612d77719514ec7da23a45e26f91ba515
T0nci <138928016+T0nci@users.noreply.github.com>
2024-08-26 18:10:28 +0200

    feat: Style App component

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -1,20 +1,24 @@
+import styles from "./App.module.css";
+
 const App = () => {
   return (
     <>
       <main>
-        <header>Home</header>
-        <h1>Welcome to FakeStore!</h1>
-        <p>
-          Discover a world of curated, high-quality products designed to make
-          your life easier and more enjoyable. At FakeStore, we’re passionate
-          about bringing you the latest trends and timeless essentials at
-          unbeatable prices. Whether you’re shopping for cutting-edge gadgets,
-          stylish fashion, or unique home decor, our diverse selection ensures
-          that there’s something for everyone. Browse our collections, enjoy
-          seamless shopping experiences, and let us help you find exactly what
-          you need. Dive into FakeStore today—where exceptional finds are just a
-          click away!
-        </p>
+        <div className="container">
+          <header>Home</header>
+          <h1 className={styles.heading}>Welcome to FakeStore!</h1>
+          <p className={styles.paragraph}>
+            Discover a world of curated, high-quality products designed to make
+            your life easier and more enjoyable. At FakeStore, we’re passionate
+            about bringing you the latest trends and timeless essentials at
+            unbeatable prices. Whether you’re shopping for cutting-edge gadgets,
+            stylish fashion, or unique home decor, our diverse selection ensures
+            that there’s something for everyone. Browse our collections, enjoy
+            seamless shopping experiences, and let us help you find exactly what
+            you need. Dive into FakeStore today—where exceptional finds are just
+            a click away!
+          </p>
+        </div>
       </main>
     </>
   );
--- src/Components/App/App.test.jsx
+++ src/Components/App/App.test.jsx
@@ -14,15 +14,23 @@ describe("App component", () => {
     expect(container).toMatchInlineSnapshot(`
       <div>
         <main>
-          <header>
-            Home
-          </header>
-          <h1>
-            Welcome to FakeStore!
-          </h1>
-          <p>
-            Discover a world of curated, high-quality products designed to make your life easier and more enjoyable. At FakeStore, we’re passionate about bringing you the latest trends and timeless essentials at unbeatable prices. Whether you’re shopping for cutting-edge gadgets, stylish fashion, or unique home decor, our diverse selection ensures that there’s something for everyone. Browse our collections, enjoy seamless shopping experiences, and let us help you find exactly what you need. Dive into FakeStore today—where exceptional finds are just a click away!
-          </p>
+          <div
+            class="container"
+          >
+            <header>
+              Home
+            </header>
+            <h1
+              class="_heading_d249f9"
+            >
+              Welcome to FakeStore!
+            </h1>
+            <p
+              class="_paragraph_d249f9"
+            >
+              Discover a world of curated, high-quality products designed to make your life easier and more enjoyable. At FakeStore, we’re passionate about bringing you the latest trends and timeless essentials at unbeatable prices. Whether you’re shopping for cutting-edge gadgets, stylish fashion, or unique home decor, our diverse selection ensures that there’s something for everyone. Browse our collections, enjoy seamless shopping experiences, and let us help you find exactly what you need. Dive into FakeStore today—where exceptional finds are just a click away!
+            </p>
+          </div>
         </main>
       </div>
     `);
